684a6b8cb45556668b866c30e1524f261cac93c7
Pascal <pascalazubike003@@@gmail.com>
2024-01-19 18:12:59 +0100

    change localhost to backend deploy

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,15 +1,15 @@
 import axios from "axios";
 
-const customFetch = axios.create({
-	baseURL: "https://blogvana-backend.onrender.com/api",
-});
-
-export default customFetch;
-
 // const customFetch = axios.create({
-// 	baseURL: " http://localhost:5000/api",
+// 	baseURL: "https://blogvana-backend.onrender.com/api",
 // });
 
 // export default customFetch;
+
+const customFetch = axios.create({
+	baseURL: " http://localhost:5000/api",
+});
+
+export default customFetch;
 
 
