e49739d6e8bc1b54669e51ea98ab1f531fcc9d57
AZUBIKE PASCAL ADOH <123998334+pascal-tech1@users.noreply.github.com>
2024-04-15 14:49:56 +0100

    Update dataFormatter.js

--- frontend/src/utils/dataFormatter.js
+++ frontend/src/utils/dataFormatter.js
@@ -1,30 +1,17 @@
-export function formatDate(dateString, daysAgo = 0) {
-  // Create a copy of the input date to avoid modifying it
-  const pastDate = new Date(date.getTime());
+import moment from "moment";
+export function formatDate(dateString) {
+	const inputDate = moment(dateString, "YYYY-MM-DD"); // Assuming dateString is in the format 'YYYY-MM-DD'
+	const currentDate = moment();
 
-  // Adjust the date by the specified number of days
-  pastDate.setDate(pastDate.getDate() - daysAgo);
-
-  const today = new Date();
-
-  const diffInMs = today.getTime() - pastDate.getTime();
-  const diffInDays = Math.floor(diffInMs / (24 * 60 * 60 * 1000));
-
-  // Check if pastDate is today
-  if (diffInDays === 0) {
-    return "Today";
-  } 
-  // Check if pastDate is yesterday
-  else if (diffInDays === 1) {
-    return "Yesterday";
-  } 
-  // Check if within the past week
-  else if (diffInDays < 7) {
-    return `${diffInDays} days ago`;
-  } 
-  // Otherwise, return formatted date string
-  else {
-    const options = { year: 'numeric', month: '2-digit', day: '2-digit' };
-    return pastDate.toLocaleDateString('en-US', options);
-  }
+	if (currentDate.diff(inputDate, "days") < 1) {
+		return "Today";
+		return "today";
+	} else if (currentDate.diff(inputDate, "days") === 1) {
+		return "Yesterday";
+		return "yesterday";
+	} else if (currentDate.diff(inputDate, "days") < 7) {
+		return currentDate.diff(inputDate, "days") + " days ago";
+	} else {
+		return inputDate.format("DD MMM YYYY").toLowerCase();
+	}
 }
