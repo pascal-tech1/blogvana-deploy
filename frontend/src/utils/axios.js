0b840519fe3b6997c78499b1a07232a7abb3e3d6
AZUBIKE PASCAL ADOH <123998334+pascal-tech1@users.noreply.github.com>
2024-07-01 06:16:30 -0700

    Update axios.js

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const customFetch = axios.create({
-	 baseURL: "https://blogvana-backend.vercel.app/api",
+	 baseURL: "https://blogvana-backend.onrender.com/api",
 	
 	// baseURL: "https://blogvana-deploy-production.up.railway.app/api",
 	
